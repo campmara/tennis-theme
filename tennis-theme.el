a4f0e4c2f24d006ed569790403a40877798f857b
Mara Campbell <mara@galliumstudios.com>
2025-08-10 15:38:23 -0700

    hello

--- tennis-theme.el
+++ tennis-theme.el
@@ -59,7 +59,7 @@
    `(tooltip             ((t (:foreground ,tennis-fg :background ,tennis-bg-1 :inherit variable-pitch))))
 
    `(font-lock-builtin-face           ((t (:foreground ,tennis-cyan))))
-   `(font-lock-comment-face           ((t (:foreground ,tennis-mono-3 :slant 'normal))))
+   `(font-lock-comment-face           ((t (:foreground ,tennis-mono-3 :slant normal))))
    `(font-lock-comment-delimiter-face ((default (:inherit (font-lock-comment-face)))))
    `(font-lock-doc-face               ((t (:inherit (font-lock-string-face)))))
    `(font-lock-function-name-face     ((t (:foreground ,tennis-green))))
